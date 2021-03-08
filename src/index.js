886422ef7549976640ddbfcc07ad5581263cc26e
brunoribeiro-py <the_bruno15@hotmail.com>
2021-03-08 11:55:28 -0300

    busca de extrato implementada

--- src/index.js
+++ src/index.js
@@ -10,6 +10,14 @@ const customers = [];
 app.post('/account', (request, response) => {
   const { cpf, name } = request.body;
 
+  const customerAlreadyExists = customers.some(
+    (customer) => customer.cpf === cpf
+  );
+
+  if (customerAlreadyExists) {
+    return response.status(400).json({ error: "Customer already exists!" });
+  }
+
   const id = uuidv4();
 
   customers.push({
@@ -21,6 +29,18 @@ app.post('/account', (request, response) => {
 
   return response.status(201).send();
 
+});
+
+app.get('/statement/:cpf', (request, response) => {
+  const { cpf } = request.params;
+
+  const customer = customers.find(customer => customer.cpf === cpf);
+
+  if (!customer) {
+    return response.status(400).json({ error: "Customer not found!" });
+  }
+
+  return response.json(customer.statement);
 });
 
 app.listen(3333);
