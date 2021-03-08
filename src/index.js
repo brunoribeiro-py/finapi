ef44de9ced0c456eaeb7de93e8c12554805a56dc
brunoribeiro-py <the_bruno15@hotmail.com>
2021-03-08 14:49:03 -0300

    criada busca e atualização de dados do cliente

--- src/index.js
+++ src/index.js
@@ -112,4 +112,19 @@ app.get('/statement/date', verifyIfCPFAccountExists, (request, response) => {
   return response.json(statement);
 });
 
+app.put('/account', verifyIfCPFAccountExists, (request, response) => {
+  const { name } = request.body;
+  const { customer } = request;
+
+  customer.name = name;
+
+  return response.status(201).send();
+});
+
+app.get('/account', verifyIfCPFAccountExists, (request, response) => {
+  const { customer } = request;
+
+  return response.json(customer);
+})
+
 app.listen(3333);
